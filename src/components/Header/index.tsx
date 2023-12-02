2a5bc6c8d8d3e8a9b9fbf76e6bc5b7bb63532280
SergioJaramilloL <sergiojaramilloluna@gmail.com>
2023-12-02 12:13:16 -0500

    layout header finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import styles from './header.module.scss';
 
 import Image from 'next/image';
-
+import { FaCartShopping, FaBell, FaUser  } from "react-icons/fa6";
 
 export default function Header() {
   return(
-    <header className='header'>
-      <Image src="/exito-logo.svg" alt="logo" width={80} height={31} />
+    <header className={styles.header}>
+      <Image src="/exito-logo.svg" alt="logo" width={100} height={35} />
+      <div className={styles.header_icons}>
+        <FaUser size={20}/>
+        <FaBell size={20}/>
+        <FaCartShopping size={20}/>
+      </div>
     </header>
   )
 }
