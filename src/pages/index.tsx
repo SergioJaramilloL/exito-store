f894953877f66f0ad5b5ec0ff61b0725105cfec0
SergioJaramilloL <sergiojaramilloluna@gmail.com>
2023-12-02 23:37:24 -0500

    completed product detail page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,12 @@
-import styles  from './index.module.scss'
-
-import Head from 'next/head'
-
-import Card from '@/components/card'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Productos</title>
-        <meta name="description" content="Página donde se listan los productos" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.home}>
-        <p className={styles.home_breadcrumbs}>inicio / <strong>productos</strong></p>
-        <h1>Productos éxito</h1>
-        <div className={styles.home_gallery}>
-          {new Array(10).fill(0).map((_, index) => ( <Card key={index} />))}
-        </div>
-      </main>
-    </>
-  )
+  return <></>
+}
+
+export const getServerSideProps = async () => {
+  return  {
+    redirect: {
+      destination: '/productos',
+    }
+  }
 }
