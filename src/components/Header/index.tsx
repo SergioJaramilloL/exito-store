7d7c4b843b364c3484284345aa72df8e5c3b2f1b
SergioJaramilloL <sergiojaramilloluna@gmail.com>
2023-12-03 14:58:08 -0500

    add total products in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,11 @@ import styles from './header.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
 import { FaCartShopping, FaBell, FaUser  } from "react-icons/fa6";
+import { useSelector } from 'react-redux';
 
 export default function Header(): JSX.Element {
+  const { totalProducts } = useSelector((state: any) => state.cart);
+
   return(
     <header className={styles.header}>
       <Link href="/productos">
@@ -15,6 +18,7 @@ export default function Header(): JSX.Element {
         <FaBell size={20}/>
         <Link href="/checkout">
           <FaCartShopping size={20}/>
+          {totalProducts > 0 && <div>{totalProducts}</div>}
         </Link>
       </div>
     </header>
