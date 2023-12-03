8438aaec052048e079bea249feb63f4901e9a21b
SergioJaramilloL <sergiojaramilloluna@gmail.com>
2023-12-02 19:38:18 -0500

    created card for render product and created gallery layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import styles  from './index.module.scss'
+
 import Head from 'next/head'
+
+import Card from '@/components/card'
 
 export default function Home() {
   return (
@@ -9,7 +13,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Home page</h1>
+      <main className={styles.home}>
+        <p className={styles.home_breadcrumbs}>inicio / <strong>productos</strong></p>
+        <h1>Productos éxito</h1>
+        <div className={styles.home_gallery}>
+          {new Array(10).fill(0).map((_, index) => ( <Card key={index} />))}
+        </div>
+      </main>
     </>
   )
 }
